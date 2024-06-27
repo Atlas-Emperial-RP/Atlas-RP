5202b9a981bc05a34c51e2b0a0cac86fca43ea60
yairme <yairstward2013@gmail.com>
2024-06-27 23:49:35 +0200

    Remove unused files and directories

--- garrysmod/addons/auto_training_v1.82/lua/entities/training_npc/shared.lua
+++
@@ -1,9 +0,0 @@
-ENT.Base = "base_anim"
-ENT.Type = "ai"
-ENT.Base = "base_gmodentity"
-
-ENT.PrintName = "Training NPC"
-ENT.AuthorName = "Nicolas"
-ENT.Category = "[RDV] Training"
-ENT.Spawnable = true
-ENT.RenderGroup = RENDERGROUP_BOTH
