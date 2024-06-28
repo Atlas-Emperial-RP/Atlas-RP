2d5acc20521d014fbd3582b28685bdaa1823954d
yairme <yairstward2013@gmail.com>
2024-06-28 02:02:37 +0200

    Removed what wasn't needed

--- garrysmod/addons/ch_medals/lua/ch_medals/server/ch_medals_init.lua
+++
@@ -1,8 +0,0 @@
-util.AddNetworkString( "CH_Medals_Net_OpenMedalsMenu" )
-util.AddNetworkString( "CH_Medals_Net_HUDPaintLoad" )
-util.AddNetworkString( "CH_Medals_Net_NetworkMedals" )
-util.AddNetworkString( "CH_Medals_Net_GiveMedal" )
-util.AddNetworkString( "CH_Medals_Net_ShowHideMedal" )
-util.AddNetworkString( "CH_Medals_Net_TakeMedal" )
-
-resource.AddWorkshop( "3113792066" )
